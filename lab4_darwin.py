7fc206703acbe5a1705f22722ccbd43ff65e10f1
Darwin Guglielmo <71992769+darwingug@users.noreply.github.com>
2020-10-14 14:09:40 -0700

    Update lab4_darwin.py

--- lab4_darwin.py
+++ lab4_darwin.py
@@ -4,13 +4,17 @@
 import random 
 #this will help us with the randomization
 def main():
-  board = [
-    [X, X, X, X, X],
-    [X, X, X, X, X],
-    [X, X, X, X, X],
-    [X, X, X, X, X],
-    [X, X, X, X, X]
-]
+  board = def touch(board, row, col):
+    white_square = "\N{'WHITE SQUARE'}"
+    black_square = "\N{'BLACK SQUARE'}"
+    print(board[row][col])
+    
+    if board[row][col] == white_square:
+      print(board[row][col]
+    elif board[row][col] == black_square
+            print('inserting black square')
+    else:
+            print('Oops! Try Again!')
 
 randomize(board)
     moves = 0
@@ -24,39 +28,4 @@ randomize(board)
 count = getCount()
 lights = get_lights(count)
 
-def square():
-  square = "\N{'WHITE SQUARE','BLACK SQUARE'}"
-
-def get_lights(count):
-  return [random.choice("\N{'WHITE SQUARE','BLACK SQUARE'}" for i in range(count)]
-
-def getcount():
-  while True:
-    else:
-      count=int(input('How many lights would you like to have? '))
-            return count
- 
-def getLightToToggle(count):
-    while True:
-        else:
-            index=int(input('Which light do you want to toggle (0 to quit)?  '))
-            if 0<=index and index<=count:return index
-            else:print('Index out of range')
-                        
-def toggleLights(index,lights):
-    if lights[index - 1] == "\N{'WHITE SQUARE'}": lights[index - 1] = "\N{'BLACK SQUARE'}"
-    else:lights[index - 1] = "\N{'WHITE SQUARE'}"
-
-    if index ==1:
-        if lights[1] == "\N{'WHITE SQUARE'}": lights[1] = "\N{'BLACK SQUARE'}"
-        else: lights[1] = "\N{'WHITE SQUARE'}"
-    elif index==len(lights):
-        if lights[index-2] == "\N{'WHITE SQUARE'}": lights[index-2] = "\N{'BLACK SQUARE'}"
-        else: lights[index-2] = "\N{'WHITE SQUARE'}"
-    else:
-        if lights[index-2]=="\N{'WHITE SQUARE'}":lights[index-2]="\N{'BLACK SQUARE'}"
-        else:lights[index-2]="\N{'WHITE SQUARE'}"
-        if lights[index]=="\N{'WHITE SQUARE'}":lights[index]="\N{'BLACK SQUARE'}"
-        else:lights[index]="\N{'WHITE SQUARE'}"
-                 
 main()
